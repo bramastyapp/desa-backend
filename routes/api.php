d297e775b47a258b4636bc0e83bcc276a5da5ac1
bramastyapp <bramastyapp@gmail.com>
2024-09-23 18:14:59 +0700

    feat: API dashboard

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,15 @@ Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'in
 
 //group route with middleware "auth"
 Route::group(['middleware' => 'auth:api'], function () {
-
     //logout
     Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+});
+
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    //group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+        //dashboard
+        Route::get('/dashboard', App\Http\Controllers\Api\Admin\DashboardController::class);
+    });
 });
