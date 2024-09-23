d63cfefa8653b450de554d6ae6088e5af55bf54c
bramastyapp <bramastyapp@gmail.com>
2024-09-23 18:10:38 +0700

    feat: API login, logout

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+//route login
+Route::post('/login', [App\Http\Controllers\Api\Auth\LoginController::class, 'index']);
+
+//group route with middleware "auth"
+Route::group(['middleware' => 'auth:api'], function () {
+
+    //logout
+    Route::post('/logout', [App\Http\Controllers\Api\Auth\LoginController::class, 'logout']);
+});
